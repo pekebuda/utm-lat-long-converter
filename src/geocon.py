d6454b6fa8813bdf8ed8c35cde518ed01875e695
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2018-10-24 12:46:16 +0200

    Convierte en constantes de modulo ciertas propiedades de clase.

--- src/geocon.py
+++ src/geocon.py
@@ -1,17 +1,16 @@
 from math import sqrt, sin, cos, tan, floor, pi
+K = 1
+K0 = 0.9996
+DRAD = pi / 180
+DIAGRAPH_LETTERS_E = "ABCDEFGHJKLMNPQRSTUVWXYZ"
+DIAGRAPH_LETTERS_N = "ABCDEFGHJKLMNPQRSTUV"
+DIAGRAPH_LETTERS_ALL = "ABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUV"
 
 
 class Geocon:
     '''
     Main class
     '''
-    # constants used in calculations
-    k = 1
-    k0 = 0.9996
-    drad = pi / 180
-    digraphLettersE = "ABCDEFGHJKLMNPQRSTUVWXYZ"
-    digraphLettersN = "ABCDEFGHJKLMNPQRSTUV"
-    digraphLettersAll = "ABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUVABCDEFGHJKLMNPQRSTUV"
     datumTable = [
         {'eqRad': 6378137.0, 'flat': 298.2572236},    # WGS 84
         {'eqRad': 6378137.0, 'flat': 298.2572236},    # NAD 83
@@ -54,8 +53,8 @@ class Geocon:
         this function assumes that all data validation has been performed prior to calling
         it.
         '''
-        phi = lat * self.drad                           # convert latitude to radians
-        lng = lngd * self.drad                          # convert longitude to radians
+        phi = lat * DRAD                           # convert latitude to radians
+        lng = lngd * DRAD                          # convert longitude to radians
         utmz = 1 + floor((lngd + 180) / 6)              # longitude to utm zone
         zcm = 3 + 6 * (utmz - 1) - 180                  # central meridian of a zone
         latz = 0                                        # this gives us zone A-B for below 80S
@@ -77,7 +76,7 @@ class Geocon:
         N = self.a / sqrt(1 - (self.e * sin(phi))**2)
         T = tan(phi)**2
         C = e0sq * cos(phi)**2
-        A = (lngd - zcm) * self.drad * cos(phi)
+        A = (lngd - zcm) * DRAD * cos(phi)
 
         # calculate M (USGS style)
         M = phi * (1 - esq * (1 / 4 + esq * (3 / 64 + 5 * esq / 256)))
@@ -90,11 +89,11 @@ class Geocon:
 
         # now we are ready to calculate the UTM values...
         # first the easting
-        x = self.k0 * N * A * (1 + A * A * ((1 - T + C) / 6 + A * A * (5 - 18 * T + T * T + 72 * C - 58 * e0sq) / 120))  # Easting relative to CM
+        x = K0 * N * A * (1 + A * A * ((1 - T + C) / 6 + A * A * (5 - 18 * T + T * T + 72 * C - 58 * e0sq) / 120))  # Easting relative to CM
         x = x + 500000  # standard easting
 
         # now the northing
-        y = self.k0 * (M - M0 + N * tan(phi) * (A * A * (1 / 2 + A * A * ((5 - T + 9 * C + 4 * C * C) / 24 + A * A * (61 - 58 * T + T * T + 600 * C - 330 * e0sq) / 720))))   # first from the equator
+        y = K0 * (M - M0 + N * tan(phi) * (A * A * (1 / 2 + A * A * ((5 - T + 9 * C + 4 * C * C) / 24 + A * A * (61 - 58 * T + T * T + 600 * C - 330 * e0sq) / 720))))   # first from the equator
         yg = y + 10000000  # yg = y global, from S. Pole
         if (y < 0):
             y = 10000000 + y   # add in false northing if south of the equator
@@ -110,7 +109,7 @@ class Geocon:
             'nato': {
                 'easting': round(10*(x-100000*floor(x/100000)))/10,
                 'northing': round(10*(y-100000*floor(y/100000)))/10,
-                'latZone': self.digraphLettersN[latz],
+                'latZone': DIAGRAPH_LETTERS_N[latz],
                 'lngZone': utmz,
                 'digraph': digraph
             }
@@ -137,9 +136,9 @@ class Geocon:
         M = 0
 
         if (not southern):
-            M = M0 + y / self.k0    # Arc length along standard meridian.
+            M = M0 + y / K0    # Arc length along standard meridian.
         else:
-            M = M0 + (y - 10000000) / self.k
+            M = M0 + (y - 10000000) / K
 
         mu = M / (self.a * (1 - esq * (1 / 4 + esq * (3 / 64 + 5 * esq / 256))))
         phi1 = mu + e1 * (3 / 2 - 27 * e1 * e1 / 32) * sin(2 * mu) + e1 * e1 * (21 / 16 - 55 * e1 * e1 / 32) * sin(4 * mu)   # Footprint Latitude
@@ -148,14 +147,14 @@ class Geocon:
         T1 = tan(phi1)**2
         N1 = self.a / sqrt(1 - (self.e * sin(phi1))**2)
         R1 = N1 * (1 - self.e**2) / (1 - ((self.e * sin(phi1))**2))
-        D = (x - 500000) / (N1 * self.k0)
+        D = (x - 500000) / (N1 * K0)
         phi = (D * D) * (1 / 2 - D * D * (5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * e0sq) / 24)
         phi = phi + D**6 * (61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * e0sq - 3 * C1 * C1) / 720
         phi = phi1 - (N1 * tan(phi1) / R1) * phi
 
-        lat = floor(1000000 * phi / self.drad) / 1000000
+        lat = floor(1000000 * phi / DRAD) / 1000000
         lng = D * (1 + D * D * ((-1 - 2 * T1 - C1) / 6 + D * D * (5 - 2 * C1 + 28 * T1 - 3 * C1 * C1 + 8 * e0sq + 24 * T1 * T1) / 120)) / cos(phi1)
-        lng = lngd = zcm + lng / self.drad
+        lng = lngd = zcm + lng / DRAD
 
         return {
             'lat': lat,
@@ -206,15 +205,15 @@ class Geocon:
         if (nltr == "W" or nltr == "X" or nltr == "Y" or nltr == "Z"):
             raise ValueError("W, X, Y and Z are not legal second characters in a digraph")
 
-        eidx = self.digraphLettersE.indexOf(eltr)
-        nidx = self.digraphLettersN.indexOf(nltr)
+        eidx = DIAGRAPH_LETTERS_E.indexOf(eltr)
+        nidx = DIAGRAPH_LETTERS_N.indexOf(nltr)
 
         if (utmz / 2 == floor(utmz / 2)):
             nidx -= 5  # correction for even numbered zones
 
         ebase = 100000*(1 + eidx - 8 * floor(eidx / 8))
 
-        latBand = self.digraphLettersE.indexOf(latz)
+        latBand = DIAGRAPH_LETTERS_E.indexOf(latz)
         latBandLow = 8 * latBand - 96
         latBandHigh = 8 * latBand - 88
 
@@ -232,9 +231,9 @@ class Geocon:
         highLetter = round(100 + 1.11 * latBandHigh)
         latBandLetters = None
         if (utmz / 2 == floor(utmz / 2)):
-            latBandLetters = self.digraphLettersAll.slice(lowLetter + 5, highLetter + 5)
+            latBandLetters = DIAGRAPH_LETTERS_ALL.slice(lowLetter + 5, highLetter + 5)
         else:
-            latBandLetters = self.digraphLettersAll.slice(lowLetter, highLetter)
+            latBandLetters = DIAGRAPH_LETTERS_ALL.slice(lowLetter, highLetter)
         nbase = 100000 * (lowLetter + latBandLetters.indexOf(nltr))
 
         x = ebase + utme
@@ -271,9 +270,9 @@ class Geocon:
         M0 = 0
 
         if (not southern):
-            M = M0 + y / self.k0    # Arc length along standard meridian.
+            M = M0 + y / K0    # Arc length along standard meridian.
         else:
-            M = M0 + (y - 10000000) / self.k
+            M = M0 + (y - 10000000) / K
 
         # calculate the latitude so that we can derive the latitude zone
         mu = M / (self.a * (1 - esq * (1 / 4 + esq * (3 / 64 + 5 * esq / 256))))
@@ -283,11 +282,11 @@ class Geocon:
         T1 = tan(phi1)**2
         N1 = self.a / sqrt(1 - (self.e * sin(phi1))**2)
         R1 = N1 * (1 - self.e**2) / (1 - (self.e * sin(phi1))**2)
-        D = (x - 500000) / (N1 * self.k0)
+        D = (x - 500000) / (N1 * K0)
         phi = (D * D) * (1 / 2 - D * D * (5 + 3 * T1 + 10 * C1 - 4 * C1 * C1 - 9 * e0sq) / 24)
         phi = phi + D**6 * (61 + 90 * T1 + 298 * C1 + 45 * T1 * T1 - 252 * e0sq - 3 * C1 * C1) / 720
         phi = phi1 - (N1 * tan(phi1) / R1) * phi
-        lat = floor(1000000 * phi / self.drad) / 1000000
+        lat = floor(1000000 * phi / DRAD) / 1000000
 
         # convert latitude to latitude zone for NATO
         if (lat > -80 and lat < 72):            # zones C-W in this range
@@ -304,7 +303,7 @@ class Geocon:
         return {
             'easting': x,
             'northing': y,
-            'latZone': self.digraphLettersN[latz],
+            'latZone': DIAGRAPH_LETTERS_N[latz],
             'lngZone': utmz,
             'digraph': digraph
         }
@@ -321,12 +320,12 @@ class Geocon:
         # first get the east digraph letter
         letter = floor((utmz - 1) * 8 + (x) / 100000)
         letter = letter - 24 * floor(letter / 24) - 1
-        digraph = self.digraphLettersE.charAt(letter)
+        digraph = DIAGRAPH_LETTERS_E.charAt(letter)
 
         letter = floor(y / 100000)
         if (utmz / 2 == floor(utmz / 2)):
             letter = letter + 5
         letter = letter - 20 * floor(letter / 20)
-        digraph = digraph + self.digraphLettersN.charAt(letter)
+        digraph = digraph + DIAGRAPH_LETTERS_N.charAt(letter)
 
         return digraph
