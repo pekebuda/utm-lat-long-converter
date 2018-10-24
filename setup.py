112758f51ba4b3c5721a27e34ce8c6567cbc4c5c
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2018-10-24 19:09:08 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='geocon',
-    version='0.0.1',
+    version='0.0.2',
     description='A UTM -> Lat/Long (or vice versa) converter.',
     # long_description=open('README.rst').read(),
     url='https://github.com/pekebuda/utm-lat-long-converter-python',
